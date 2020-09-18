645fc15b9e4fe6eea9cec6bb8ad805e74d11792f
Dave Parfitt <dparfitt@mozilla.com>
2020-09-17 20:41:07 -0400

    wip

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,23 @@
 module Main (main) where
-
+import Options.Applicative
+import Data.Semigroup ((<>))
+import Data.List (intercalate)
 import Moa.Moa (someFunc)
 import Moa.CLI
 
+
 main :: IO ()
-main = do
-         let x = sample
-         putStrLn "MOA v0.1.0"
-         someFunc
+main = greet =<< execParser opts
+  where
+    opts = info (cli_opts <**> helper)
+      ( fullDesc
+     <> progDesc "moa v0.1.0"
+     <> header "moa - a compiler" )
+
+greet :: CLIOpts -> IO ()
+greet (CLIOpts ins out _quiet _verbosity) = 
+        do
+          putStrLn $ "Writing output to " ++ out
+          putStrLn $ "Compiling " ++ filelist
+        where
+          filelist = intercalate ", " ins
--- src/Moa/CLI.hs
+++ src/Moa/CLI.hs
@@ -1,25 +1,30 @@
-module Moa.CLI (Sample(..), sample) where
+module Moa.CLI (CLIOpts(..), cli_opts) where
 import Options.Applicative
 --import Data.Semigroup ((<>))
 
-data Sample = Sample
-  { hello      :: String
+data CLIOpts = CLIOpts
+  { infiles    :: [String]
+  , output     :: String
   , quiet      :: Bool
   , enthusiasm :: Int }
 
-sample :: Parser Sample
-sample = Sample
-      <$> strOption
-          ( long "hello"
-         <> metavar "TARGET"
-         <> help "Target for the greeting" )
+cli_opts :: Parser CLIOpts
+cli_opts = CLIOpts
+      <$> some (argument str (metavar "FILES..."))
+      <*> strOption
+        ( long "output"
+        <> short 'o'
+        <> metavar "FILE"
+        <> value "a.out"
+        <> showDefault
+        <> help "Write output to FILE" )      
       <*> switch
           ( long "quiet"
          <> short 'q'
          <> help "Whether to be quiet" )
       <*> option auto
-          ( long "enthusiasm"
-         <> help "How enthusiastically to greet"
+          ( long "verbosity"
+         <> help "Noisy compilation"
          <> showDefault
          <> value 1
          <> metavar "INT" )
